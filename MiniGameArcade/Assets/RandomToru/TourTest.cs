70a898a905edbec6e5221d7d17ec86747729e7cb
keyoni <56132773+keyoni@users.noreply.github.com>
2022-04-27 19:13:18 -0700

    Practice stuff

---
+++ MiniGameArcade/Assets/RandomToru/TourTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourTest : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    private void Awake()
+    {
+       DontDestroyOnLoad(this);
+         
+    }
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- MiniGameArcade/Assets/StartMenu/Scripts/MenuNavagation.cs
+++ MiniGameArcade/Assets/StartMenu/Scripts/MenuNavagation.cs
@@ -1,25 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuNavagation : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SceneChange(String sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
 }
+
