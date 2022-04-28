a4a40d013c453aec12592090aadd1c8fe535c56a
keyoni <56132773+keyoni@users.noreply.github.com>
2022-04-27 20:39:33 -0700

    little fix

--- MiniGameArcade/Assets/NextLevel.cs
+++ MiniGameArcade/Assets/NextLevel.cs
@@ -7,7 +7,7 @@ public class NextLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if()
+       
     }
 
     // Update is called once per frame
