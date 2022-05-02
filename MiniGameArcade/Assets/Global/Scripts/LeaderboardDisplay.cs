e5145a4651a94bda26680ab1faba394886f3a457
keyoni <56132773+keyoni@users.noreply.github.com>
2022-05-01 22:30:07 -0700

    Leaderboard Starting
    Todo: shift leader, make menu, make for other games, add arrows to switch between games

---
+++ MiniGameArcade/Assets/Global/Scripts/LeaderboardDisplay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LeaderboardDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text gameTitle;
+    [SerializeField] private TMP_Text firstName;
+    [SerializeField] private TMP_Text firstScore;
+    [SerializeField] private TMP_Text secondName;
+    [SerializeField] private TMP_Text secondScore;
+    [SerializeField] private TMP_Text thirdName;
+    [SerializeField] private TMP_Text thirdScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public  void DisplayLeaderboard(LeaderboardScriptable lb)
+    {
+        gameTitle.text = lb.levelTitle;
+        
+        firstName.text =  PlayerPrefs.GetString(lb.playerPrefsBase + "[1].name"); 
+        firstScore.text = PlayerPrefs.GetInt(lb.playerPrefsBase + "[1].score").ToString();
+        secondName.text = PlayerPrefs.GetString(lb.playerPrefsBase + "[2].name"); 
+        secondScore.text = PlayerPrefs.GetInt(lb.playerPrefsBase + "[2].score").ToString();
+        thirdName.text = PlayerPrefs.GetString(lb.playerPrefsBase + "[2].name");
+        thirdScore.text = PlayerPrefs.GetInt(lb.playerPrefsBase + "[2].score").ToString();
+        
+    }
+}
